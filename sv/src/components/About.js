9920d0e5837016ff4f10565dc8d47fa814330610
khmelnitskyd <94955716+khmelnitskyd@users.noreply.github.com>
2023-01-17 14:17:01 +0300

    init: create about

---
+++ sv/src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+    return (
+        <section class="about">
+
+            <h3>Company history</h3>
+            <p>Our company was founded in 2022 under the strict guidance of Denis Khmelnitsky from ASOIR-201.</p>
+
+            <h3>Our employees</h3>
+            <p>Our staff is able to impress anyone. At the moment, one person is employed in the company.</p>
+
+            <h3>Company Results</h3>
+            <p>10K tickets are sold daily with the help of our service. The company's market capitalization is estimated at one million Iranian rials.</p>
+
+        </section>
+    )
+}
