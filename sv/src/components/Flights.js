27fa1e8870bef87728c4f5765fc7756b15f474a6
khmelnitskyd <94955716+khmelnitskyd@users.noreply.github.com>
2023-01-17 14:21:23 +0300

    fix: ticket display

--- sv/src/components/Flights.js
+++ sv/src/components/Flights.js
@@ -1,47 +1,81 @@
-import React from "react";
+import React, {useState, useEffect} from 'react'
+import TicketService from '../services/TicketService';
+import TableBody from '@mui/material/TableBody'
+import TableRow from '@mui/material/TableRow'
 import "./Flights.css";
 
-export default function Flights() {
-    return (
-        <main className="Flights">
-        <section className="Ticket">
-        <div class="cardWrap">
-  <div class="card cardLeft">
-    <div class = "cardHeader">
-    <h1>Let't <span>Traveling</span></h1>
-    </div>
-    <div class="titlel">
-      <span>From</span>
-      <h2>New York</h2>
-    </div>
-    <div class="name">
-    <span>To</span>
-      <h2>Paris</h2>
-      
-    </div>
-    <div class="seat">
-    <span>Departure </span>
-      <h2>20.03.2000</h2>
-      
-    </div>
-    <div class="time">
-    <span>time</span>
-      <h2>12:00</h2>
-      
-    </div>
-    
-  </div>
-  <div class="card cardRight">
-    <div class="eye"></div>
-    <div class="number">
-      <h3>180</h3>
-      <span>tickets</span>
-    </div>
-    <div class="barcode"></div>
-  </div>
+const Flights = () => {
 
-</div>
-        </section>
-        </main>
+    const [flights, setFlights] = useState([])
+
+    useEffect(() => {
+
+        getAllFlights();
+    }, [])
+
+    const getAllFlights = () => {
+        TicketService.getAllFlights().then((response) => {
+            setFlights(response.data)
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+
+    return (
+        
+      <main className="Flights"> 
+      <TableBody>
+                    {flights.map(flight =>
+                        <TableRow className="TableRow" key={flight.id}>
+      <section className="Ticket">
+        <div class="cardWrap">
+          <div class="card cardLeft">
+            <div class="cardHeader">
+              <h1>Let't <span>Traveling</span></h1>
+            </div>
+            <div class="titlel">
+              <span>From</span>
+              <h2>{flight.from}</h2>
+            </div>
+            <div class="name">
+              <span>To</span>
+              <h2>{flight.to}</h2>
+
+            </div>
+            <div class="seat">
+              <span>Departure </span>
+              <h2>{flight.departureTime}</h2>
+
+            </div>
+            <div class="time">
+              <span>time</span>
+              <h2>12:00</h2>
+
+            </div>
+
+          </div>
+          <div class="card cardRight">
+            <div class="eye"></div>
+            <div class="number">
+              <h3>{flight.countTickets}</h3>
+              <span>tickets</span>
+            </div>
+            <div class="barcode"></div>
+          </div>
+
+        </div>
+      </section>
+         
+                        </TableRow>)}
+                </TableBody>
+    </main>
+        
+                
+         
     )
 }
+
+export default Flights;
+
