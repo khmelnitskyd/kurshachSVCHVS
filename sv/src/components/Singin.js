dec4ae4043fab9d71a98f4914c2889bb1feb55ac
khmelnitskyd <94955716+khmelnitskyd@users.noreply.github.com>
2023-01-17 14:19:15 +0300

    fix: add authorization

--- sv/src/components/Singin.js
+++ sv/src/components/Singin.js
@@ -1,26 +1,61 @@
-import React from "react";
+import React, {useState} from 'react'
 import "./Singup.css";
+import UserService from "../services/UserService";
 
-export default function Singup() {
+export default function Signin() {
+
+    const [userName, setuserName] = useState('')
+    const [password, setpassword] = useState('')
+    const [check, setcheck] = useState('')
+
+    const searchUser = (userName) => {
+        UserService.getUserByUsername(userName).then((response) => {
+
+            console.log(response.data);
+            
+           if(String(check.password)===password)
+           {
+            alert("Вы успешно авторизировались")
+           }
+           else
+           {
+            alert(check.password+"     "+password)
+            
+           }
+           
+        }).catch(error => {
+            console.log(error);
+            alert("false")
+        })
+    }
+
+    const tempCheck = (userName) => {
+        UserService.getUserByUsername(userName).then((response) => {
+            setcheck(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
     return (
-        <section className="Singup">
+        <section className="Singup" onMouseMove={(e) => tempCheck(userName)}>
 
             
                 <div class="title">Welcome!</div>
                 <div class="group">
-                    <input type="text" required/>
+                    <input type="text" value={userName}  onChange={(e) => setuserName(e.target.value)}   required/>
                         <span class="highlight"></span>
                         <span class="bar"></span>
                         <label>User</label>
                 </div>
                 
                 <div class="group">
-                    <input type="password" required/>
+                    <input type="password" value={password}  onChange={(e) => setpassword(e.target.value)}  required/>
                         <span class="highlight"></span>
                         <span class="bar"></span>
                         <label>Password</label>
                 </div>
-                <button className="btnSubmit">Submit</button>
+                <button className="btnSubmit"  onClick={() => searchUser(userName)}>Submit</button>
                 
             
         </section>
