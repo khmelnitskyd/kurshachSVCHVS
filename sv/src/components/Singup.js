f67bc662edf72dc70572cdc454815073ee610b5a
khmelnitskyd <94955716+khmelnitskyd@users.noreply.github.com>
2023-01-17 14:18:32 +0300

    fix: add registration

--- sv/src/components/Singup.js
+++ sv/src/components/Singup.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, {useState} from 'react'
+
 import "./Singup.css";
+import UserService from "../services/UserService";
 
 export default function Singup() {
+
+    const [userName, setuserName] = useState('')
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+
+    
+
+    const saveOrUpdateEmployee = (e) => {
+        e.preventDefault();
+
+        const user = {userName,email,password}
+
+       
+        UserService.createUser(user).then((response) => {
+
+                console.log(response.data)
+                alert("Вы успешно зарегистрировались")
+                
+
+            }).catch(error => {
+                console.log(error)
+                alert("Произошла ошибка, имя пользователя возможно занято")
+            })
+        
+
+    }
+
     return (
         <section className="Singup">
 
@@ -11,24 +40,24 @@ export default function Singup() {
 
                 <div class="subtitle">Let's create your account!</div>
                 <div class="group">
-                    <input type="text" required/>
+                    <input type="text" value={userName}  onChange={(e) => setuserName(e.target.value)} required/>
                         <span class="highlight"></span>
                         <span class="bar"></span>
                         <label>User</label>
                 </div>
                 <div class="group">
-                    <input type="text" required/>
+                    <input type="text" value={email}  onChange={(e) => setemail(e.target.value)} required/>
                         <span class="highlight"></span>
                         <span class="bar"></span>
                         <label>E-mail</label>
                 </div>
                 <div class="group">
-                    <input type="password" required/>
+                    <input type="password" value={password}  onChange={(e) => setpassword(e.target.value)} required/>
                         <span class="highlight"></span>
                         <span class="bar"></span>
                         <label>Password</label>
                 </div>
-                <button className="btnSubmit">Submit</button>
+                <button className="btnSubmit" onClick={(e) => saveOrUpdateEmployee(e)}>Submit</button>
             
         </section>
     )
